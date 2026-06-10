9a85ba5e5b7d74842745e037b39b9e1b6529bcdb
nasirkirmanni <nasirkirmani1@gmail.com>
2026-06-10 13:03:23 +0530

    Clean up temp scripts

--- backend/src/updateSeed.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-let content = fs.readFileSync('c:/Users/nasir/OneDrive/Desktop/Food App/backend/src/data/seedData.js', 'utf8');
-
-content = content.replace(/image:\s*"https:\/\/images\.unsplash\.com[^"]+"/g, 'image: "/images/restaurants/restaurant-art.png"');
-
-fs.writeFileSync('c:/Users/nasir/OneDrive/Desktop/Food App/backend/src/data/seedData.js', content);
-console.log("Updated seedData.js");
